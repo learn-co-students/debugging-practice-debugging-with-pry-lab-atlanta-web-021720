e3432a9f78cf576c41e59531471576d8e3ab6215
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-02-12 14:43:32 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,21 @@
 require 'pry'
 
-def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+def start_game (player1, player2)
+  player1 = "Rocky"
+  player2 = "Bullwinkle"
+  "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+
+
+def play_game(player1,player2)
+  player1 = "Jay-Z"
+  player2 = "Beyonce"
   "#{player1} is better than #{player2}"
+end
+
+describe "the_final_frontier" do
+  it "`engage` should return the captain's log and crew greetings" do
+    expect(engage).to eq(["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."])
+  end
 end
